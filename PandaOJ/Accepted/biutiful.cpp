bff8af7c5564f48c12135e56d579a3e3c76155f5
krossis <57306362+krossis@users.noreply.github.com>
2019-11-09 14:34:04 +0700

    Add files via upload

---
+++ PandaOJ/Accepted/biutiful.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std; 
+int main(){
+int t,i,tot,masuk;
+scanf("%d",&t);
+string s[t]={};
+for(i=0;i<t;i++)
+{
+    scanf("%d",&masuk);
+    int r=1;
+    tot=0;
+    while(tot<masuk){
+        tot+=r;
+        r+=1;
+        if(tot==masuk){
+            s[i]="YES";
+            break;
+        }
+        if(tot>masuk){
+            s[i]="NO";
+            break;
+        }
+
+
+    }
+
+}
+
+for(i=0;i<t;i++){
+    cout<<"Case #"<<i+1<<": "<<s[i]<<"\n";
+
+}
+
+}
